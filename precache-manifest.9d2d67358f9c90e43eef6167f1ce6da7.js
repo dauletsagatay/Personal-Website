1c56c4a21e11005fadee89fad9be1541a29d830d
Daulet Sagatay <d.sagatay@imbc.kz>
2022-12-06 11:52:41 +0600

    Updates

---
+++ precache-manifest.9d2d67358f9c90e43eef6167f1ce6da7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bc9b57f04620d1f5ac3",
+    "url": "/Personal-Website/static/css/main.97ce918d.chunk.css"
+  },
+  {
+    "revision": "7bc9b57f04620d1f5ac3",
+    "url": "/Personal-Website/static/js/main.7bc9b57f.chunk.js"
+  },
+  {
+    "revision": "bc61a901237da4c7c2db",
+    "url": "/Personal-Website/static/js/1.bc61a901.chunk.js"
+  },
+  {
+    "revision": "87f34ce215affaea4209",
+    "url": "/Personal-Website/static/js/runtime~main.87f34ce2.js"
+  },
+  {
+    "revision": "535d534c0b1f60f6959fb0a7d09c20ea",
+    "url": "/Personal-Website/static/media/DS_CV.535d534c.pdf"
+  },
+  {
+    "revision": "5810d2d6c183fc3d7bb892e165dc3faf",
+    "url": "/Personal-Website/static/media/about.5810d2d6.jpeg"
+  },
+  {
+    "revision": "5810d2d6c183fc3d7bb892e165dc3faf",
+    "url": "/Personal-Website/static/media/profile.5810d2d6.jpeg"
+  },
+  {
+    "revision": "f4ff40e2ca2b7c80779312e50d49ff1a",
+    "url": "/Personal-Website/index.html"
+  }
+];
